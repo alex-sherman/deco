e4613f1eea2594e1a7cff87f1031484b2de262d1
Tushar Makkar <tushar@codenation.co.in>
2016-09-13 17:03:28 +0530

    Pep8 for deco

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(
-  name = "deco",
-  version = "0.4.1",
-  description = "A decorator for concurrency",
-  packages = ["deco"],
-  author='Alex Sherman',
-  author_email='asherman1024@gmail.com',
-  url='https://github.com/alex-sherman/deco')
+    name="deco",
+    version="0.4.1",
+    description="A decorator for concurrency",
+    packages=["deco"],
+    author='Alex Sherman',
+    author_email='asherman1024@gmail.com',
+    url='https://github.com/alex-sherman/deco')
