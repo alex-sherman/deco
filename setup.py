56914a483674ab62ff63e2f2e2ca8b21b5949852
Alex Sherman <asherman1024@gmail.com>
2021-03-22 00:29:59 -0500

    Version 0.6.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
   name = "deco",
-  version = "0.6.1",
+  version = "0.6.2",
   description = "A decorator for concurrency",
   packages = ["deco"],
   author='Alex Sherman',
